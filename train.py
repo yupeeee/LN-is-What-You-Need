04c291df11c624fa606c1b2700fd81c8d83f5e44
yupeeee <juyeopkim@yonsei.ac.kr>
2024-11-14 17:07:32 +0900

    train replace add

--- train.py
+++ train.py
@@ -2,21 +2,35 @@ import argparse
 import os
 
 from paths import IMAGENET_DIR, LOGS_DIR
-from utils import ImageNetTrainer, load_config, load_model
+from utils import (
+    ImageNetTrainer,
+    load_config,
+    load_model,
+    replace_bn_with_ln,
+    replace_ln_with_bn,
+)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--bn2ln", action="store_true", default=False)
+    parser.add_argument("--ln2bn", action="store_true", default=False)
     parser.add_argument("--config", type=str, required=True)
-    parser.add_argument("--resume", action="store_true")
+    parser.add_argument("--resume", action="store_true", default=False)
     args = parser.parse_args()
 
     config = load_config(os.path.join("cfgs", args.config))
+
     model = load_model(args.model)
+    if args.bn2ln:
+        model = replace_bn_with_ln(model)
+    elif args.ln2bn:
+        model = replace_ln_with_bn(model)
+
     trainer = ImageNetTrainer(
         dataset_dir=IMAGENET_DIR,
         save_dir=LOGS_DIR,
-        name=args.model,
+        name=args.model + ("_bn2ln" if args.bn2ln else "_ln2bn" if args.ln2bn else ""),
         version=args.config,
         **config,
     )
